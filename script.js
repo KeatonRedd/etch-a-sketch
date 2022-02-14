0f0da438f073f504ffe8866126550c1dcf083d2b
KeatonRedd <keaton.redd@gmail.com>
2022-02-13 23:34:40 -0600

    finished

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
+const theGrid = document.querySelector('#theGrid');
+const gridWidth = document.getElementById('body').clientWidth / 2;
+const gridItems = theGrid.children;
+const clearBtn = document.querySelector('#clearBtn');
+const randomBtn = document.querySelector('#randomBtn')
+createGrid(4);
+
 function createGrid(squareThis) {
-    gridWidth = (document.getElementById('body').clientWidth) * .5;
-    gridHeight = gridWidth;
+    setGridContainer();
+    let squareSize = (gridWidth / squareThis);
 
     for (let i = 0; i < (squareThis ** 2); i++) {
         let newSquare = document.createElement('div');
-        newSquare.classList.add(`squareItem`);
 
-        document.getElementById('theGrid').append(newSquare);
+        newSquare.classList.add('squareItem');
+        newSquare.style.width = squareSize + 'px';
+        newSquare.style.height = squareSize + 'px';
+
+        theGrid.append(newSquare);
     }
 
-    for (let i = 0; i < squareThis; i++) {
-        const gta = document.getElementById('theGrid');
+    if (randomBtn.checked) {
+        randomMouseover();
+    }
 
-        if (gta.getAttribute('grid-template-areas') === null) {
-            gta.setAttribute('grid-template-areas', 'a ');
-            // gta.style.gridTemplateAreas = ('a ');
-        }
-
-        else {
-            gta.setAttribute('grid-template-areas', `${gta.getAttribute('grid-template-areas') + 'a '}`);
-            // gta.style.gridTemplateAreas = (`${gta.getAttribute('grid-template-area') + 'a '}`);
-        }
-
-        console.log(gta.getAttribute('grid-template-areas'));
-
+    else {
+        mouseoverListener();
     }
 }
 
-createGrid(4);
+function setGridContainer() {
+    theGrid.style.width = (gridWidth) + 'px';
+    theGrid.style.height = (gridWidth) + 'px';
+}
+
+function mouseoverListener() {
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].addEventListener('mouseover', () => {
+            gridItems[i].classList.add('colored');
+        });
+    };
+}
+
+function randomMouseover() {
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].addEventListener('mouseover', () => {
+            gridItems[i].style.backgroundColor = `${'#' + Math.floor(Math.random()*16777215).toString(16)}`;
+        })
+    }
+}
+
+function clearSquares() {
+    while (theGrid.firstChild) {
+        theGrid.removeChild(theGrid.firstChild);
+    }
+}
+
+clearBtn.addEventListener('click', () => {
+    let userInput;
+
+    do {
+        userInput = prompt("How many rows and columns would you like to have?");
+    }while (userInput > 100)
+
+    clearSquares();
+    createGrid(userInput);
+});
